78b0e5d99bca79610e583b879ee7418aac5df25a
Crispin Wellington <retrogradeorbit@gmail.com>
2016-10-31 22:45:57 +0800

    rough and ready player movement

--- src/dungeon_crawler/core.cljs
+++ src/dungeon_crawler/core.cljs
@@ -2,7 +2,11 @@
   (:require  [infinitelives.pixi.canvas :as c]
              [infinitelives.pixi.resources :as r]
              [infinitelives.pixi.tilemap :as tm]
+             [infinitelives.pixi.texture :as t]
+             [infinitelives.pixi.sprite :as s]
              [infinitelives.utils.events :as e]
+             [infinitelives.utils.vec2 :as vec2]
+             [infinitelives.utils.gamepad :as gp]
              [infinitelives.utils.console :refer [log]])
   (:require-macros [cljs.core.async.macros :refer [go]]
                    [infinitelives.pixi.macros :as m]))
@@ -130,6 +134,9 @@
     ;; load resource url with tile sheet
     (<! (r/load-resources canvas :ui ["img/tiles.png"
                                       "img/notlink.png"]))
+    (t/load-sprite-sheet!
+     (r/get-texture :notlink :nearest)
+     hero)
 
     (let [tile-set (tm/make-tile-set :tiles tile-set-mapping [16 16])
           tile-sprites (->> tile-map-chars
@@ -139,19 +146,67 @@
       (m/with-sprite :tilemap
         [tile-map (tm/make-tilemap tile-sprites
                                    :scale 2
-                                   :particle-opts #{:uvs})]
+                                   :particle-opts #{:uvs})
+         player (s/make-sprite :down-1 :scale 2 :x -100 :y -100)]
 
-        (while true
-          ;; closed
-          (<! (e/wait-frames 120))
-          (tm/alter-tile! tile-sprites [0 2] tile-set :door-left-shut-1)
-          (tm/alter-tile! tile-sprites [1 2] tile-set :door-left-shut-2)
-          (tm/alter-tile! tile-sprites [0 3] tile-set :door-left-shut-3)
-          (tm/alter-tile! tile-sprites [1 3] tile-set :door-left-shut-4)
+        ;; door opens and closes
+        (go
+          (while true
+            ;; closed
+            (<! (e/wait-frames 120))
+            (tm/alter-tile! tile-sprites [0 2] tile-set :door-left-shut-1)
+            (tm/alter-tile! tile-sprites [1 2] tile-set :door-left-shut-2)
+            (tm/alter-tile! tile-sprites [0 3] tile-set :door-left-shut-3)
+            (tm/alter-tile! tile-sprites [1 3] tile-set :door-left-shut-4)
 
-          ;; open
-          (<! (e/wait-frames 120))
-          (tm/alter-tile! tile-sprites [0 2] tile-set :door-left-1)
-          (tm/alter-tile! tile-sprites [1 2] tile-set :door-left-2)
-          (tm/alter-tile! tile-sprites [0 3] tile-set :door-left-3)
-          (tm/alter-tile! tile-sprites [1 3] tile-set :door-left-4))))))
+            ;; open
+            (<! (e/wait-frames 120))
+            (tm/alter-tile! tile-sprites [0 2] tile-set :door-left-1)
+            (tm/alter-tile! tile-sprites [1 2] tile-set :door-left-2)
+            (tm/alter-tile! tile-sprites [0 3] tile-set :door-left-3)
+            (tm/alter-tile! tile-sprites [1 3] tile-set :door-left-4)))
+
+        (loop [pos (vec2/vec2 -100 -100)
+               vel (vec2/zero)]
+          (let [
+                joy (vec2/vec2 (or (gp/axis 0)
+                                   (cond (e/is-pressed? :left) -1
+                                         (e/is-pressed? :right) 1
+                                         :default 0) )
+                               (or (gp/axis 1)
+                                   (cond (e/is-pressed? :up) -1
+                                         (e/is-pressed? :down) 1
+                                         :default 0)
+                                   ))
+                ]
+            (s/set-pos! player pos)
+
+            (case (vec2/get-x joy)
+              -1
+              (do (s/set-texture! player :right-1)
+                  (s/set-scale! player -2 2))
+              1
+              (do (s/set-texture! player :right-1)
+                  (s/set-scale! player 2 2))
+               nil
+              )
+
+            (case (vec2/get-y joy)
+              -1
+              (do (s/set-texture! player :up-1)
+                  (s/set-scale! player 2))
+              1
+              (do (s/set-texture! player :down-1)
+                  (s/set-scale! player 2))
+               nil
+              )
+
+
+            (<! (e/next-frame))
+            (recur (vec2/add pos vel)
+                   (-> vel
+                       (vec2/add joy)
+                       (vec2/scale 0.90)
+                       (vec2/truncate 3)))))
+
+        ))))
